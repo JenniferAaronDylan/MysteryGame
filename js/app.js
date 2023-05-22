c302c5f127255e670d08630b4cec9a2d84f3398b
Aaron Clark <amclarkwustl@gmail.com>
2023-05-22 14:19:57 -0700

    added about us and constructor function

---
+++ js/app.js
@@ -0,0 +1,14 @@
+'use strict'
+
+// User Constructor
+// To Do.  In submission handler for username, check local storage.  If username exists, start game from previous state.
+
+function User(username){
+  this.username = username;
+  this.Clara = false;
+  this.Marguerite = false;
+  this.Alexander = false;
+  this.Jonathan = false;
+  this.Wallace = false;
+  this.Leonard = false;
+}
