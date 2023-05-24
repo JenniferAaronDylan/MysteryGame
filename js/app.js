cb80c9b9e7e6137c60761a77ed75d6cd798c9b57
Cooper <cooper.softdev@gmail.com>
2023-05-23 18:12:18 -0800

    css glow, new story para, display function

--- js/app.js
+++ js/app.js
@@ -243,12 +243,12 @@ const claraDeveraux = {
   image: '../img/clara.png',
 
   evidence: 'Found paint chips on her dress that match the color palette of the stolen painting.',
-  additionalEvidence: 'Clearly disgruntled by my interview, the paint chips are allegedly from a piece she did to network herself more commissions. There is also a sketch of the stolen painting found in her sketchbook, suggesting she had a keen interest in it, but enough of an interest to steal it? Her resentment toward Van Dyke is palpable, but her artistic admiration for the stolen painting makes her involvement in its theft implausible.',
+  additionalEvidence: 'Clearly disgruntled by my interview, the paint chips are allegedly from a piece she did to network herself more commissions. There is also a sketch of the stolen painting found in her sketchbook, suggesting she had a keen interest in it, but enough of an interest to steal it? Her resentment toward Van is palpable, but her artistic admiration for the stolen painting makes her involvement in its theft implausible.',
 
   alibi: 'Was seen in the dining area before the estimated time of the murder, but not eating.',
   additionalAlibi: 'She claims she was at the dinner party until near midnight then retired to her room, naming the Steward as an alibi. My assistant interviewed him and he corroborates her story. She noted that he seemed honest, and we have no reason at this time to believe he\'s being untruthful. She left me with a cryptic warning as I left, she may know more than she\'s leading on.',
 
-  assistantNotes: 'Clara held a grudge against Van Dyke for trying to ruin her career. Is that enough of a motive? And does she have a strong alibi?',
+  assistantNotes: 'Clara held a grudge against Van for trying to ruin her career. Is that enough of a motive? And does she have a strong alibi?',
 
   accusationExplanation: 'Clara\'s resentment towards Van is clear, and her interest in the painting is undeniable. However, she has a strong alibi. The steward vouches for her presence in her quarters during the estimated time of the murder. As well as her passion for art which seems more towards creation rather than destruction or theft. Her guilt is highly unlikely.',
 
@@ -256,7 +256,7 @@ const claraDeveraux = {
     'Good morning, Miss Deveraux, do you mind if I asked you some questions, maybe take a look around your quarters? I\'m sure you\'ve heard about what happened last night.',
     'I did hear, what a tragic loss. And certainly, make yourself at home, it seems you\'re going to regardless.',
     'Can you explain those paint chips on your dress to me? It appears you\'ve been handling a dry painting.',
-    'Oh my, what a keen eye... but maybe not so much a keen mind? I\'m a struggling artist darling... I\'ve got to pay my way somehow, after Van Dyke tried to destroy my career.',
+    'Oh my, what a keen eye... but maybe not so much a keen mind? I\'m a struggling artist darling... I\'ve got to pay my way somehow, after Van tried to destroy my career.',
     'Paying your way by stealing priceless paintings, maybe?',
     'Hardly... I heard Alexander Petrov was going to be aboard, and since he\'s on the committee overseeing the new National Art Exhibit I figured I could grease the wheels to get prime space of the gallery. So I did a portrait of his wife and daughter.',
     'Why is there a sketch of the stolen painting in your sketchbook then?',
@@ -272,37 +272,11 @@ const claraDeveraux = {
     'Can anyone vouch for you\'r whereabouts after you left the party?',
     'I\'m sure the steward would recall, maybe ask him? He escorted me back.',
     'Thank you for your cooperation, Miss Deveraux.',
-    'I don\'t think this clears me of any suspicion, does it?... Leonard Van Dyke is hardly the man he portrays to everyone else. I\'m sure you\'ll see that soon enough.',
+    'I don\'t think this clears me of any suspicion, does it?... Leonard Van is hardly the man he portrays to everyone else. I\'m sure you\'ll see that soon enough.',
   ],
 
-  // Displays the conversation based on the question asked
-  displayConversation: function(conversation) {
-    const playerSpeechElement = document.getElementById('playerSpeech');
-    const characterSpeechElement = document.getElementById('characterSpeech');
-    playerSpeechElement.textContent = '';
-    characterSpeechElement.textContent = '';
-  
-    let index = 0;
-  
-    function displayNextLine() {
-      if (index < conversation.length) {
-        if (index % 2 === 0) {
-          playerSpeechElement.textContent += conversation[index];
-        } else {
-          characterSpeechElement.textContent += conversation[index];
-        }
-        index++;
-        setTimeout(displayNextLine, getRandomDelay());
-      }
-    }
-    displayNextLine();
-  },
 };
 
-// Natural speech pattern generator
-function getRandomDelay() {
-  return Math.floor(Math.random() * 1000) + 500;
-}
 
 // Clara HTML Buttons
 <div>
@@ -320,18 +294,6 @@ const askForAlibiButton = document.getElementById('askForAlibi');
 const searchForEvidenceButton = document.getElementById('searchForEvidence');
 const accuseClaraButton = document.getElementById('accuseClara');
 
-askForAlibiButton.addEventListener('click', function() {
-  claraDeveraux.unlockResponse('askForAlibi');
-});
-
-searchForEvidenceButton.addEventListener('click', function() {
-  claraDeveraux.unlockResponse('searchForEvidence');
-});
-
-accuseClaraButton.addEventListener('click', function() {
-  claraDeveraux.updateAccusationStatus(true);
-});
-
 document.getElementById('askForAlibiButton').addEventListener('click', function() {
   claraDeveraux.displayConversation(claraDeveraux.alibiConversation);
 });
@@ -340,3 +302,32 @@ document.getElementById('searchForEvidenceButton').addEventListener('click', fun
   claraDeveraux.displayConversation(claraDeveraux.evidenceConversation);
 });
 
+  // Displays the conversation based on the question asked
+function displayConversation(conversation) {
+  const playerSpeechElement = document.getElementById('playerSpeech');
+  const characterSpeechElement = document.getElementById('characterSpeech');
+  playerSpeechElement.textContent = '';
+  characterSpeechElement.textContent = '';
+
+  let index = 0;
+
+  function displayNextLine() {
+    if (index < conversation.length) {
+      if (index % 2 === 0) {
+        playerSpeechElement.textContent += conversation[index];
+      } else {
+        characterSpeechElement.textContent += conversation[index];
+      }
+      index++;
+      setTimeout(displayNextLine, getRandomDelay());
+    } else {
+      playerSpeechElement.textContent = playerSpeech;
+      characterSpeechElement.textContent = characterSpeech;
+    }
+  }
+  displayNextLine();
+  // Natural speech pattern generator
+  function getRandomDelay() {
+    return Math.floor(Math.random() * 1000) + 500;
+  }
+}
